1a787bf1725a8bad236080965e139b2ea0c23890
“zhangmm <823772544@qq.com>
2022-10-15 18:48:19 +0800

    first commit

---
+++ netty-rpc-client/src/main/java/com/netty/rpc/client/connect/SuperTest.java
@@ -0,0 +1,4 @@
+package com.netty.rpc.client.connect;
+
+public class SuperTest {
+}
---
+++ netty-rpc-client/src/main/java/com/netty/rpc/client/proxy/RpcService.java
@@ -0,0 +1,18 @@
+package com.netty.rpc.client.proxy;
+
+import com.netty.rpc.client.handler.RpcFuture;
+
+/**
+ * Created by zhangmm on 2016/3/16.
+ *
+ * @author g-yu
+ */
+public interface RpcService<T, P, FN extends SerializableFunction<T>> {
+    RpcFuture call(String funcName, Object... args) throws Exception;
+
+    /**
+     * lambda method reference
+     */
+    RpcFuture call(FN fn, Object... args) throws Exception;
+
+}
